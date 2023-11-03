c0010d3538661f9937efc6cd4ff8b4951dab0767
raincolor <1037428213@qq.com>
2023-11-03 16:04:51 +0800

    feat: 优化

--- src/bin.ts
+++ src/bin.ts
@@ -1,35 +1,36 @@
 #!/usr/bin/env node
-import p = require('path')
-import h = require('./')
+import p = require("path");
+import h = require("./");
 
 // Show usage and exit with code
 function help(code: number) {
   console.log(`Usage:
   h-com-linters install
   h-com-linters uninstall
-  `)
-  process.exit(code)
+  `);
+  process.exit(code);
 }
 
 // Get CLI arguments
-const [, , cmd, ...args] = process.argv
-const ln = args.length
-const [x] = args
+// process.argv属性会返回一个数组，其中包含当 Node.js 进程被启动时传入的命令行参数。第一个元素是 process.execPath。 第二个元素是正被执行的 JavaScript 文件的路径。 其余的元素是任何额外的命令行参数。
+const [, , cmd, ...args] = process.argv;
+const ln = args.length;
+const [x] = args;
 
 // CLI commands
 const cmds: { [key: string]: () => void } = {
   install: (): void => (ln > 1 ? help(2) : h.install(x)),
   uninstall: h.uninstall,
-  ['-v']: () =>
+  ["-v"]: () =>
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-    console.log(require(p.join(__dirname, '../package.json')).version),
-}
+    console.log(require(p.join(__dirname, "../package.json")).version),
+};
 
 // Run CLI
 try {
   // Run command or show usage for unknown command
-  cmds[cmd] ? cmds[cmd]() : help(0)
+  cmds[cmd] ? cmds[cmd]() : help(0);
 } catch (e) {
-  console.error(e instanceof Error ? `h-com-linters - ${e.message}` : e)
-  process.exit(1)
+  console.error(e instanceof Error ? `h-com-linters - ${e.message}` : e);
+  process.exit(1);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,57 @@
-import cp = require('child_process')
-import fs = require('fs')
-import p = require('path')
+import cp = require("child_process");
+import fs = require("fs");
+import p = require("path");
 
 // Logger
-const l = (msg: string): void => console.log(`h-com-linters - ${msg}`)
+const l = (msg: string): void => console.log(`h-com-linters - ${msg}`);
 
 //npx command
 const npx = (args: string[]): cp.SpawnSyncReturns<Buffer> =>
-  cp.spawnSync('npx', args, { stdio: 'inherit', shell: true })
+  // child_process.spawnSync() 同步方法， 方法与 [child_process.spawn()]（异步） 基本相同，除了该方法直到子进程完全关闭后才返回。
+  cp.spawnSync("npx", args, { stdio: "inherit", shell: true });
 
-const notCopyWhenExistRule = /tsconfig\.json$/
+const notCopyWhenExistRule = /tsconfig\.json$/;
 
-export function install(dir = '.'): void {
+export function install(dir = "."): void {
   // Ensure that we're inside a git repository
   // If git command is not found, status is null and we should return.
   // That's why status value needs to be checked explicitly.
-  if (npx(['husky', 'install']).status !== 0) {
-    l(`npx command not found, skipping install`)
-    return
+  if (npx(["husky", "install"]).status !== 0) {
+    l(`npx command not found, skipping install`);
+    return;
   }
 
   try {
     fs.cp(
-      p.join(__dirname, '../configs'),
+      p.join(__dirname, "../configs"),
       p.join(dir),
       {
         recursive: true,
         filter(source, destination) {
+          // 不能直接覆盖原有的tsconfig。json
           if (notCopyWhenExistRule.test(source) && fs.existsSync(destination)) {
             l(
-              'tsconfig.json already exists, if you want to use the file in h-com-linters, please delete it first',
-            )
-            return false
+              "tsconfig.json already exists, if you want to use the file in h-com-linters, please delete it first"
+            );
+            return false;
           }
-          return true
+          return true;
         },
       },
       (err) => {
         if (err) {
-          throw err
+          throw err;
         }
-      },
-    )
+      }
+    );
   } catch (e) {
-    l('h-com-linters failed to install')
-    throw e
+    l("h-com-linters failed to install");
+    throw e;
   }
 
-  l('h-com-linters installed')
+  l("h-com-linters installed");
 }
 
 export function uninstall(): void {
-  npx(['husky', 'uninstall'])
+  npx(["husky", "uninstall"]);
 }
